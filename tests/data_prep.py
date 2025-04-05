2b65c92addd72ed87c1804a35ff37fba2ef4fe67
DylanTHebert <dth10422@gmail.com>
2025-04-05 10:58:47 -0500

    some initial project outlines

---
+++ tests/data_prep.py
@@ -0,0 +1,8 @@
+def test_file_parse():
+    pass
+
+def test_sentence_embedding():
+    pass
+
+def test_instructor_xl_embedding():
+    pass
---
+++ vector_db_service/service.py
@@ -0,0 +1,7 @@
+def create_vector_store(data_source):
+    """Creates a Vector Store from a given location on disk"""
+    pass
+
+def search_active_vector_store(embedded_search: str):
+    """Searches the vector store for closest match to the pre-embedded vector"""
+    pass
